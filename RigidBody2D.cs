be027b561872dcd18bc37425b5b0f2cdc851ea76
Pakalavan Logan <plogan@uwaterloo.ca>
2021-02-24 14:27:51 -0500

    Please

--- RigidBody2D.cs
+++ RigidBody2D.cs
@@ -2,33 +2,40 @@ using Godot;
 using System;
 public class RigidBody2D : Godot.RigidBody2D
 {
-    public float TorForce = 20;
+    
    public float CentralImpulseX = 0;
-   public double CentralImpulseY = -0.452;
+   public double ImpulseY;
    public float FrictionX = 0;
    public double FrictionY = 2.492;
-    public float torque = 2000;
+    public float torque;
+    public float torque2;
     private Timer timer;
-    public float lifetime = 10;
+    public float lifetime = 30;
     private Vector2 OriginalPosition;
     public float DistanceTravelled;
+    public float delta;
     // 1Godot mass = 1000kg, 1pixel = 0.1m, 1pixel/s = 0.1m/s, 1Godot Force Unit = 100N, 1Godot Impulse = 100Kg*m/s
 
 
     public override void _Ready()
     {
-        
+      
     }
 
     
     public override void _UnhandledInput(InputEvent @MouseEvent)
     {
+        torque = GlobalVar.torque;
+        ImpulseY = GlobalVar.Impulse;
+        torque2 = GlobalVar.torque2;
+       
         if (@MouseEvent is InputEventMouseButton mouseclick)
         {
             //IMPULSE AND APPLIEDFORCE
             if (!mouseclick.Pressed && mouseclick.ButtonIndex ==(int)ButtonList.Right)
             {
-                this.ApplyCentralImpulse(new Vector2(0,-250));
+                this.ApplyCentralImpulse(new Vector2(0,(float)ImpulseY));
+                
                            
             }
 
@@ -36,8 +43,20 @@ public class RigidBody2D : Godot.RigidBody2D
            //TORQUE
            if (!mouseclick.Pressed && mouseclick.ButtonIndex ==(int)ButtonList.Right)
            {
-               rotationDir += 1;
-               this.ApplyTorqueImpulse(rotationDir * torque);
+               if (torque > 0)
+               {
+                   rotationDir += 1;
+                   this.ApplyTorqueImpulse(rotationDir * torque);
+               }
+               
+               if (torque2 > 0)
+               {
+                   rotationDir += 1;
+                    this.ApplyTorqueImpulse(rotationDir * -torque2);
+               }
+               
+               
+               
            } 
             
             //LIFETIME
@@ -54,6 +73,7 @@ public class RigidBody2D : Godot.RigidBody2D
            if (!mouseclick.Pressed && mouseclick.ButtonIndex ==(int)ButtonList.Right)
            {
                OriginalPosition = this.Position;
+                
            }
 
         }
@@ -66,27 +86,77 @@ public class RigidBody2D : Godot.RigidBody2D
 
     public override void _PhysicsProcess(float delta)
     {
-        if (this.LinearVelocity.y < 0)
-        {
-            this.AppliedForce = (new Vector2(5,50));
-        }
-            
+        //FRICTION AND CURL NEEDS WORK TO MAKE ACCURATE
         this.AppliedTorque = ((float)(-0.1*this.AngularVelocity));
         GlobalVar.LinearVelocity = this.LinearVelocity.y;
         GlobalVar.AngularVelocity = this.AngularVelocity;
-
-        if (this.LinearVelocity.y == 0)
+        FrictionY = GlobalVar.friction;
+        
+        
+        //COUNTERCLOCKWISE
+        
+        if (this.AngularVelocity < 0)
         {
-            this.AppliedForce = (new Vector2(0,0));
-            this.LinearVelocity = (new Vector2(0,0));
+                                  
+            if (this.LinearVelocity.y < 0)
+            {
+                this.AppliedForce = (new Vector2(-5,(float)FrictionY));
+            }
+            
+            if (this.LinearVelocity.y == 0)
+            {
+                this.AppliedForce = (new Vector2(0,0));
+                this.LinearVelocity = (new Vector2(0,0));
+            }
+
+            if (this.LinearVelocity.y > 0)
+            {
+                this.AppliedForce = (new Vector2(0,0));
+                this.LinearVelocity = (new Vector2(0,0));
+            }
         }
 
-        if (this.LinearVelocity.y > 0)
+        if (this.AngularVelocity == 0)
         {
-            this.AppliedForce = (new Vector2(0,0));
-            this.LinearVelocity = (new Vector2(0,0));
-        }
+            if (this.LinearVelocity.y < 0)
+            {
+                this.AppliedForce = (new Vector2(0,(float)FrictionY));
+            }
 
+            if (this.LinearVelocity.y == 0)
+            {
+                this.AppliedForce = (new Vector2(0,0));
+                this.LinearVelocity = (new Vector2(0,0));
+
+            }
+
+            if (this.LinearVelocity.y > 0)
+            {
+                this.AppliedForce = (new Vector2(0,0));
+                this.LinearVelocity = (new Vector2(0,0));
+            }
+        }
+        
+        //CLOCKWISE
+        if (this.AngularVelocity > 0)
+        {
+             if (this.LinearVelocity.y < 0)
+            {
+                this.AppliedForce = (new Vector2(5,(float)FrictionY));
+            }
+            
+            if (this.LinearVelocity.y == 0)
+            {
+                this.AppliedForce = (new Vector2(0,0));
+                this.LinearVelocity = (new Vector2(0,0));
+            }
+
+            if (this.LinearVelocity.y > 0)
+            {
+                this.AppliedForce = (new Vector2(0,0));
+                this.LinearVelocity = (new Vector2(0,0));
+            }
+        }
         //CALCULATE DISTANCE TRAVELLED
         
         float DistanceTravelled = this.Position.DistanceTo(this.OriginalPosition);
